bfd6264d3e4c1290b2a8b7703df5a390d3acd577
Theo <ranalan.theo123@gmail.com>
2025-07-03 03:38:08 +0800

    fixed password problem

--- users/UserController.js
+++ users/UserController.js
@@ -26,7 +26,7 @@ async function RegisterUser(req,res) {
     if (password.replace(/ /g, "").length < 8) return res.status(400).json({ error: 'password too short' });
 
 
-    const hash = await bcrypt.hash(password, 10);
+    const hash = await bcrypt.hash(password.replace(/ /g, ""), 10);
     const user = await User.create({name, role, birthDate, email, password: hash });
 
     res.status(201).json({ message: 'User Registered', userID: user.id });
