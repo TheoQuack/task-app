3d60f740a3144d05a468c923c9baa5b118338134
Theo <ranalan.theo123@gmail.com>
2025-07-15 01:54:39 +0800

    Changed the config to refer to the env file

--- config/database.js
+++ config/database.js
@@ -1,9 +1,15 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize('task_manager_theo', 'theo', 'theo', {
-    host: process.env.DATABASE,
+// const sequelize = new Sequelize('task_manager_theo', 'theo', 'theo', {
+//     host: process.env.DATABASE,
+//     dialect: 'mysql'
+// });
+
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USER , process.env.PASSWORD , {
+    host: process.env.HOST,
     dialect: 'mysql'
 });
+
 
 module.exports = sequelize;
